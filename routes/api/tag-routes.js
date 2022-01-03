9734ec2e561ece8e767d53c78a60f33892b21803
SantiBass <sibarraryan@gmail.com>
2022-01-03 09:51:35 -0800

    working on product routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,24 +10,69 @@ router.get('/', (req, res) => {
     include: [{
       model: Product
     }],
-  }).then((tags)=> res.json())
+  }).then((tags)=> res.json(tags))
+  .catch((err) => { 
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id:req.params.id
+    },
+    imclude: [Product]
+  }).then((tags) => res.json(tags))
+  .catch((err) => {
+    console.log(err);
+  res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    where: {
+      id:req.params.id
+    },
+    imclude: [Product]
+  }).then((tags) => res.json(tags))
+  .catch((err) => {
+    console.log(err);
+  res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    where: {
+      id:req.params.id
+    },
+    imclude: [Product]
+  }).then((tags) => res.json(tags))
+  .catch((err) => {
+    console.log(err);
+  res.status(500).json(err);
+  });
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id:req.params.id
+    },
+    imclude: [Product]
+  }).then((tags) => res.json(tags))
+  .catch((err) => {
+    console.log(err);
+  res.status(500).json(err);
+  });
 });
 
 module.exports = router;
