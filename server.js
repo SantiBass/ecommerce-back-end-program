4c17e1c9446b6a3f5904319a935f82916072dc1a
SantiBass <sibarraryan@gmail.com>
2022-01-04 10:34:00 -0800

    working on testing

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const routes = require('./routes');
 const sequelize = require('./config/connection')
 // import sequelize connection
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({force: true}).then(()=>{
